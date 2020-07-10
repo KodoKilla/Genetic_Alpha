af5300e97114f717dc40937e86c2406d15af7ad8
KodoKilla <Tramontinenzo@gmail.com>
2020-07-09 23:07:00 -0300

    Adicionado Fitness

--- main_teste.py
+++ main_teste.py
@@ -48,11 +48,12 @@ def divide_dataset(col, dataset):
 
     return 1
 
-def get_coluna(col, individuo):
-    coluna = random.randint(0, col)
-    individuo.append(coluna)
+def get_coluna(dataframe ,col, individuo):
+    for coluna in range(0, col):
+        individuo.append(coluna)
+        get_valor(dataframe, coluna, individuo)
 
-    return coluna
+    return True
 
 def get_valor(dataframe, coluna, individuo):
     dataframe = dataframe[[str(coluna)]]
@@ -75,36 +76,44 @@ def inicializa_pop(dataframe, df_teste, col, pessoas, ultima_coluna):
     individuos = []
     for i in range(0, pessoas):
         individuo = []
-        coluna = get_coluna(col, individuo)
-        valores = get_valor(dataframe, coluna, individuo)
+        get_coluna(dataframe, col, individuo)
         previsao = get_preview(ultima_coluna, individuo)
-        traduz_individuo(individuo, valores, previsao)
+        traduz_individuo(dataframe, individuo, previsao)
 
-        fit = fitness(df_teste, individuo)
+        fit = fitness(dataframe, individuo)
         individuo.append(fit)
         if(i == 0):
             melhor = individuo
-        if(individuo[3] > melhor[3]):
+        if(individuo[len(individuo)-1] > melhor[len(individuo)-1]):
             melhor = individuo
         individuos.append(individuo)
     print('Melhor: ',melhor)
 
+    # print(individuos[1][1]) --> 1 Individuo - Coluna 2
     for i in range(0, len(individuos)):
         print(get_par(individuos))
     return 1
 
-def traduz_individuo(individuo, valores, previsao):
-    individuo[1] = valores[individuo[1]]
-    individuo[2] = previsao[individuo[2]]
+def traduz_individuo(df, individuo, previsao):
+    for i in range(0, 6):
+        dataframe = df[[str(i)]]
+        valores = pd.unique(dataframe.to_numpy().reshape(-1))
+        individuo[i+(i+1)] = valores[individuo[i+(i+1)]]
+    individuo[12] = previsao[individuo[12]]
 
 def fitness(df_teste, individuo):
     acertos = 0
-    for i in range(len(df_teste)):
-        if(df_teste.iloc[i, individuo[0]] == individuo[1]):
-            classificacao = individuo[2]
-        else:
-            classificacao = 'v-good'
-        if(classificacao == df_teste.iloc[i, df_teste.shape[1] - 1]):
+    total_carac = 0
+    for x in range(len(df_teste)):
+        classificacao = 1
+        total_carac = 0
+        for i in range(0, 6):
+            if(df_teste.iloc[x, individuo[i*2]] == individuo[i+(i+1)]):
+                classificacao = individuo[12]
+                total_carac += 1
+        if(classificacao == 1):
+            classificacao = '1'
+        if(classificacao == df_teste.iloc[x, df_teste.shape[1] - 1] and total_carac == 6):
             acertos += 1
 
     fitness = (acertos / len(df_teste)) * 100
@@ -130,7 +139,7 @@ def main():
     df = pd.DataFrame(pd.read_csv(dados))
     ultima_coluna = df.iloc[:, -1]
     df_teste = df[round((len(df) - (len(df)/3))):len(df)]
-    qtd_Colunas = df.shape[1] - 2 #Removendo a coluna a ser prevista
+    qtd_Colunas = df.shape[1] - 1 #Removendo a coluna a ser prevista
 
     inicializa_pop(df, df_teste, qtd_Colunas, tamanhopop, ultima_coluna)
 
